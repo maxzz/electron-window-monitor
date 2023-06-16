a6299e1c5b9368b59fd3329ba45dde906e611516
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-16 16:00:13 -0700

    WIP. add load/save window position

--- src/electron/app/create-main-window.ts
+++ src/electron/app/create-main-window.ts
@@ -1,5 +1,6 @@
 import path from 'node:path';
 import { BrowserWindow, app } from "electron";
+import { getIniOptions, saveIniOptions } from './utils/app-ini-options';
 
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
 const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL'];
@@ -7,28 +8,36 @@ const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL'];
 let winApp: BrowserWindow | null;
 
 export async function createWindow() {
+    const iniOptions = getIniOptions();
+
     winApp = new BrowserWindow({
+        title: 'PMAT Monitor',
+        ...(iniOptions?.bounds),
+        show: false,
         icon: path.join(process.env.PUBLIC, 'electron-vite.svg'),
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
         },
     });
 
-    // Test active push message to Renderer-process.
-    winApp.webContents.on('did-finish-load', () => {
-        winApp?.webContents.send('main-process-message', (new Date).toLocaleString());
-    });
-
     if (VITE_DEV_SERVER_URL) {
         winApp.loadURL(VITE_DEV_SERVER_URL);
     } else {
-        // win.loadFile('dist/index.html')
         winApp.loadFile(path.join(process.env.DIST, 'index.html'));
     }
 
+    winApp.once('ready-to-show', () => winApp?.show());
+
+    winApp.on('close', () => {
+        winApp && saveIniOptions(winApp);
+    });
+
+    winApp.webContents.on('did-finish-load', () => {
+        winApp?.webContents.send('main-process-message', (new Date).toLocaleString()); // Test active push message to Renderer-process.
+    });
+
     // const res = await getTargetWindow({});
     // console.log('res', res);
-
 }
 
 export function connect_MainWindowListeners() {

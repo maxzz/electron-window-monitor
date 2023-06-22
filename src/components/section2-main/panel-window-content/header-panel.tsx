7f3f73c916ed572f4a58fd15b9d4246afd040359
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-21 17:12:37 -0700

    WIP. HeaderPanel. alternative ButtonCopyContent feedback animation done even more

--- src/components/section2-main/panel-window-content/header-panel.tsx
+++ src/components/section2-main/panel-window-content/header-panel.tsx
@@ -55,8 +55,8 @@ const borderClasses = `border-primary-500 border rounded ${"hover:bg-primary-500
 
 function MountCopyNotice({ show, setShow, items }: { show: boolean; setShow?: (v: boolean) => void; items: ReactNode[]; }) {
     const transitions = useTransition(Number(show), {
-        from: { opacity: 1, y: 0, },
-        enter: { opacity: 1, y: 0, config: { duration: 0 }, },
+        from: { opacity: 0, y: 0, },
+        enter: { opacity: 1, y: 0, config: { duration: 300 }, },
         leave: { opacity: 0, y: -25, config: { duration: 350, easing: easings.easeOutQuad }, },
         onRest: ({ finished }) => show && finished && setShow?.(false),
     });
@@ -84,11 +84,10 @@ function ButtonCopyContent() {
                 show={showNotice}
                 setShow={setShowNotice}
                 items={[
-                    <div className="absolute -left-5 -top-12 text-xs opacity-25">💎</div>,
-                    <div className="absolute -left-5 -top-12 px-2 py-1 w-fit text-xs text-white bg-primary-600 border-primary-800 border rounded">Copied</div>
+                    '', // <div className="absolute -left-5 -top-12 text-xs opacity-25">💎</div>,
+                    <div className="absolute -left-5 -top-[50px] px-2 py-1 w-fit text-xs text-white bg-primary-600 border-primary-800 border rounded">Copied</div>
                 ]}
             />
-
         </button>
     );
 }

4976940ff7c16f92ffba12f430cc00bb3f86f00d
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-22 15:23:22 -0700

    WIP. SawHandlePanel. fix layout shift

--- src/components/section2-main/panel-monitor.tsx
+++ src/components/section2-main/panel-monitor.tsx
@@ -1,36 +1,15 @@
 import { useAtomValue } from "jotai";
-import { useSnapshot } from 'valtio';
 import { monitoringCounterAtom } from "@/store";
-import { clientState } from "@/store/app-state";
-
-const buttonClasses = "px-2 py-1 border-primary-500 hover:border-primary-600 hover:bg-primary-500 disabled:opacity-20 border rounded shadow active:scale-[.97] transition-transform";
 
 export function MonitoringCounter() {
     const monitoringCounter = useAtomValue(monitoringCounterAtom);
-    const { buildCounter } = useSnapshot(clientState);
     if (monitoringCounter < 0) {
         return null;
     }
     return (
-        <div className="my-4 absolute left-0 bottom-0">
-            <div className="flex flex-col justify-start">
-                <div className="text-3xl font-mono font-semibold text-transparent [-webkit-text-stroke-width:1px] [-webkit-text-stroke-color:#0da50d]">
-                    {`${monitoringCounter}`.padStart(2, '0')}
-                </div>
-
-                <div className="text-xs text-primary-700 flex items-center gap-x-2">
-                    <div className="pl-0.5">
-                        controls detection progress {buildCounter}
-                    </div>
-                    <button
-                        className={buttonClasses}
-                        onClick={() => {
-                            clientState.buildCounter = buildCounter + 1;
-                        }}
-                    >
-                        Cancel
-                    </button>
-                </div>
+        <div className="absolute left-0 -bottom-4">
+            <div className="text-3xl font-mono font-semibold text-transparent [-webkit-text-stroke-width:1px] [-webkit-text-stroke-color:#0da50d]">
+                {`${monitoringCounter}`.padStart(2, '0')}
             </div>
         </div>
     );
--- src/components/section2-main/panel-window-handle/index.tsx
+++ src/components/section2-main/panel-window-handle/index.tsx
@@ -1,13 +1,6 @@
 import { useSetAtom, useAtomValue } from "jotai";
-import { doClearSawHandleAtom, sawHandleStrAtom, sawHandleAtom } from "@/store";
+import { doClearSawHandleAtom, sawHandleStrAtom, sawHandleAtom, doMonitoringAtom } from "@/store";
 import { classNames } from "@/utils";
-
-function RowWindowInfo({ name, value, className, highlight }: { name: string; value: string; className?: string; highlight?: boolean; }) {
-    return (<>
-        <div className="px-2 py-1.5 h-full border-primary-500 border-b text-xs ">{name}</div>
-        <div className={classNames("py-1.5 border-primary-500 border-l border-b px-2", highlight && value && "bg-primary-300", className)}>{value}</div>
-    </>);
-}
 
 const borderClasses = `px-2 py-1 text-xs border-primary-500 border rounded ${"hover:bg-primary-500 select-none shadow-sm"}`;
 
@@ -24,7 +17,7 @@ function ButtonClearHandle() {
     );
 }
 
-function ButtonShowRaw({ raw }: { raw: string; }) {
+function ButtonRaw({ raw }: { raw: string; }) {
     return (
         <div className="relative group cursor-default">
             <div className={borderClasses}>Raw</div>
@@ -36,28 +29,39 @@ function ButtonShowRaw({ raw }: { raw: string; }) {
 }
 
 function HeaderButtons() {
+    const isMonitoring = useAtomValue(doMonitoringAtom);
     const raw = useAtomValue(sawHandleStrAtom);
     if (!raw) {
         return null;
     }
     return (
-        <div className="flex items-center space-x-1">
-            <ButtonClearHandle />
-            <ButtonShowRaw raw={raw} />
+        <div className="pb-1 h-7 max-w-3xl flex items-center justify-between">
+            <div className="font-semibold">
+                Second Window
+            </div>
+
+            {!isMonitoring &&
+                <div className="flex items-center space-x-1">
+                    <ButtonClearHandle />
+                    <ButtonRaw raw={raw} />
+                </div>
+            }
         </div>
     );
+}
+
+function RowWindowInfo({ name, value, className, highlight }: { name: string; value: string; className?: string; highlight?: boolean; }) {
+    return (<>
+        <div className="px-2 py-1.5 h-full border-primary-500 border-b text-xs ">{name}</div>
+        <div className={classNames("py-1.5 border-primary-500 border-l border-b px-2", className, highlight && value && "bg-primary-300/30")}>{value}</div>
+    </>);
 }
 
 export function SawHandlePanel() {
     const secondActiveWindow = useAtomValue(sawHandleAtom);
     return (
         <div className="my-4 w-full max-w-xl">
-            <div className="max-w-3xl pt-4 pb-1 flex items-center justify-between">
-                <div className="font-semibold">
-                    Second Window
-                </div>
-                <HeaderButtons />
-            </div>
+            <HeaderButtons />
 
             {secondActiveWindow && (
                 <div className="text-xs border-primary-500 border rounded grid grid-cols-[auto_1fr]">

267dc9eed742e15f616d7faed637da714bc733ad
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-24 19:05:02 -0700

    WIP. ContentScrollArea. fix columns overflow

--- src/components/section2-main/panel-window-content/content-items.tsx
+++ src/components/section2-main/panel-window-content/content-items.tsx
@@ -5,10 +5,10 @@ export function ControlsGridItems({ controls }: { controls: EngineControl[]; }) 
     return (<>
         {controls.map((control, idx) => (
             <Fragment key={idx}>
-                <div className="text-end !pl-0 border-none">{control.memid}</div>
+                <div className="text-end !pl-1 border-none">{control.memid}</div>
                 <div className="text-end">{control.orderid}</div>
                 <div className="">{control.type}</div>
-                <div className="w-max">{control.dispname}</div>
+                <div className="max-w-[30ch] whitespace-nowrap overflow-hidden overflow-ellipsis">{control.dispname}</div>
                 <div className="text-[.6rem]">{control.path}</div>
             </Fragment>
         ))}

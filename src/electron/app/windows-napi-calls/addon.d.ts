a45f7547ba879f4cd97084fb89c1197b97b9f4ef
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-01 15:56:39 -0700

    WIP. plugin. bug: Rollup failed to resolve import. 20. still no

--- src/electron/app/windows-napi-calls/addon.d.ts
+++ src/electron/app/windows-napi-calls/addon.d.ts
@@ -1,10 +1,10 @@
 // declare module "*pmat_plugin_nodejs.node" {
-declare namespace PMAT_PLUGIN_NODEJS {
+//declare namespace PMAT_PLUGIN_NODEJS {
     
     type PluginErrorCallback = (err: string) => void;
     type PluginDataCallback = (err: string, data: string) => void;
 
-    async function getTargetWindow(dataIn: object | string, cb: PluginDataCallback): Promise<string>;
+    declare async function getTargetWindow(dataIn: object | string, cb: PluginDataCallback): Promise<string>;
     
     declare class CWindowControlsCollector {
         constructor();
@@ -12,15 +12,15 @@ declare namespace PMAT_PLUGIN_NODEJS {
         cancel();
     }
 
-    async function initGdi(cd: PluginErrorCallback);
+    declare async function initGdi(cd: PluginErrorCallback);
 
-    async function getWindowIcon(paramsStr: string, cb: PluginDataCallback);
+    declare async function getWindowIcon(paramsStr: string, cb: PluginDataCallback);
 
-    async function termGdi(cb: () => void);
+    declare async function termGdi(cb: () => void);
         
     declare class CManifestForWindowCreator {
         constructor();
         async create(param: string, cb: PluginDataCallback): Promise<string>;
         cancel();
     }
-}
+//}

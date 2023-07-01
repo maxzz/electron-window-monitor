454217fb81a14ca2f25083ebc36af81bb58c3235
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-01 16:24:00 -0700

    WIP. plugin. bug: Rollup failed to resolve import. 22. still no

--- src/electron/app/windows-napi-calls/index.ts
+++ src/electron/app/windows-napi-calls/index.ts
@@ -1,80 +1,10 @@
 import { createRequire } from 'module'; //console.log(`window-monitor.ts:import.meta.url = "${import.meta.url}"`);
+
 const require = createRequire(import.meta.url);
-
-/* * @type import('./addon') */
-
-//export const addon = require('./plugins/pmat_plugin_nodejs');
-
-// import * as addon2 from './plugins/pmat_plugin_nodejs.node';
-// export const addon = addon2;
-
-// import * as addon2 from './plugins/pmat_plugin_nodejs.node';
-// export const addon = addon2;
-
-/* * @type import('./addon.d.ts') */
-//export const addon = require('./plugins/pmat_plugin_nodejs');
-
-
-
-// module Addon {
-
-//     export type PluginErrorCallback = (err: string) => void;
-//     export type PluginDataCallback = (err: string, data: string) => void;
-
-//     export declare function getTargetWindow(dataIn: object | string, cb: PluginDataCallback): Promise<string>;
-
-//     // declare class CWindowControlsCollector {
-//     //     // CWindowControlsCollector();
-//     //     collect(param: string, cb: PluginDataCallback): Promise<string>;
-//     //     cancel();
-//     // }
-
-//     export declare function initGdi(cd: PluginErrorCallback): void;
-
-//     export declare function getWindowIcon(paramsStr: string, cb: PluginDataCallback): void;
-
-//     export declare function termGdi(cb: () => void): void;
-
-//     // declare class CManifestForWindowCreator {
-//     //     CWindowControlsCollector();
-//     //     async create(param: string, cb: PluginDataCallback): Promise<string>;
-//     //     cancel();
-//     // }
-// }
-
-// export const addon = require('./plugins/pmat_plugin_nodejs') as Addon;
-
-// /<reference path="./addon.d.ts" />
-
-//export const addon = require('./plugins/pmat_plugin_nodejs.node');
-
-
-// import addon2 = require('./plugins/pmat_plugin_nodejs.node');
-// import * as addon2 = require('./plugins/pmat_plugin_nodejs.node');
-// export const addon = addon2;
-
-
-// import addon2 from './plugins/pmat_plugin_nodejs.node';
-// export const addon = addon2;
-
-
-// /// <reference types="./addon.d.ts" />
 // export const addon = require('./plugins/pmat_plugin_nodejs');
-// console.log('addon2', addon);
+export const addon2 = require('./plugins/pmat_plugin_nodejs');
 
-type Addons = {
-    // initGdi: PMAT_PLUGIN_NODEJS,
-    // termGdi: [Function (anonymous)],
-    // getWindowIcon: [Function (anonymous)],
-    getTargetWindow: PMAT_PLUGIN_NODEJS.,
-    // CWindowControlsCollector: [Function: CWindowControlsCollector],
-    // CManifestForWindowCreator: [Function: CManifestForWindowCreator];
-};
-
-
-
-export const addon = require('./plugins/pmat_plugin_nodejs');
-
+export const addon = addon2 as PluginErrorCallback;
 
 export * from './get-active-window';
 export * from './get-window-content';

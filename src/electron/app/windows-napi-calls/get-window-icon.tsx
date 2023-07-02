8796767e30031505cae2c3ed9aed39c2bff35eb2
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-02 14:18:50 -0700

    WIP. getIcon(). add save test icon.

--- src/electron/app/windows-napi-calls/get-window-icon.tsx
+++ src/electron/app/windows-napi-calls/get-window-icon.tsx
@@ -1,36 +1,38 @@
 import { addon } from ".";
-import { WindowIconGetterParams, IconFormatType, WindowIconGetter } from "./pmat-plugin-types";
-//import fs from 'fs';
+import { WindowIconGetterParams, IconFormatType, WindowIconGetter, WindowIconGetterResult } from "./pmat-plugin-types";
+import fs from 'fs';
 
-function base64Decode(str: string): Buffer {
-    return Buffer.from(str, 'base64');
-}
-
-let gManifestForWindowCreator: WindowIconGetter | null = null;
+let gWindowIconGetter: WindowIconGetter | null = null;
 
 export async function getIcon(hwnd: string, iconFormat: IconFormatType = 'png'): Promise<string> {
-    if (!gManifestForWindowCreator) {
-        gManifestForWindowCreator = new addon.WindowIconGetter();
+    if (!gWindowIconGetter) {
+        gWindowIconGetter = new addon.WindowIconGetter();
     }
 
     const params = JSON.stringify({ hwnd, iconFormat } as WindowIconGetterParams);
 
     return new Promise<string>((resolve, reject) => {
-        if (!gManifestForWindowCreator) {
-            throw new Error('no gManifestForWindowCreator');
+        if (!gWindowIconGetter) {
+            throw new Error('no gWindowIconGetter');
         }
 
-        gManifestForWindowCreator.getWindowIcon(params, (err: any, data: string) => {
+        gWindowIconGetter.getWindowIcon(params, (err: any, data: string) => {
             if (err) {
                 reject(err);
             } else {
                 resolve(data);
-                // fs.writeFileSync('ImportedIcon.' + iconFormat, iconFormat);
-
-                // const iconContentObj = JSON.parse(data);
-                // const iconBinary = base64Decode(iconContentObj.data);
-                // resolve([iconBinary, iconContentObj.type]);
+                //saveIconFile(`test-icon.${iconFormat}`, data); // icon will go to the root folder of the electron-window-monitor project.
             }
         });
     });
+}
+
+function saveIconFile(filename: string, data: string) {
+    const obj = JSON.parse(data) as WindowIconGetterResult;
+    const iconBinary = base64Decode(obj.data);
+    fs.writeFileSync(filename, iconBinary);
+
+    function base64Decode(str: string): Buffer {
+        return Buffer.from(str, 'base64');
+    }
 }

4ca4cf4925c6cd3586aa7c2eacab65d2cd4f4ec0
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-04 15:38:04 -0700

    WIP. Section2Main. PanelBuildProcess as moveable part

--- src/components/section2-main/panel-window-content/header/header-panel.tsx
+++ src/components/section2-main/panel-window-content/header/header-panel.tsx
@@ -1,52 +1,9 @@
 import { useAtomValue } from 'jotai';
 import { useSnapshot } from 'valtio';
-import { sawContentStrAtom, sendToMain } from '@/store';
+import { sawContentStrAtom } from '@/store';
 import { buildState, clientState } from "@/store/app-state";
 import { ButtonCopyContent } from './btn-copy';
-
-const buttonClasses = "px-2 py-1 border-primary-500 hover:border-primary-600 hover:bg-primary-500 border rounded shadow active:scale-[.97] disabled:scale-100 disabled:hover:bg-transparent disabled:opacity-20 transition-transform";
-
-function BuildCounter() {
-    const { buildError } = useSnapshot(clientState);
-    const { buildCounter } = useSnapshot(buildState);
-    if (buildError || buildCounter < 200) {
-        return null;
-    }
-    return (<>
-        <div className="pt-0.5">
-            controls detection progress
-        </div>
-
-        <div className="pt-0.5 min-w-[2.5rem] font-semibold font-mono">
-            {buildCounter}
-        </div>
-
-        <button className={buttonClasses} onClick={() => sendToMain({ type: 'cancel-detection' })}>
-            Cancel
-        </button>
-    </>);
-}
-
-function BuildError() {
-    const { buildError } = useSnapshot(clientState);
-    if (!buildError) {
-        return null;
-    }
-    return (
-        <div className="px-2 py-1 bg-red-600 text-white rounded-sm">
-            {buildError}
-        </div>
-    );
-}
-
-function PanelBuildProcess() {
-    return (
-        <div className="text-xs text-primary-700 flex items-center gap-x-1">
-            <BuildCounter />
-            <BuildError />
-        </div>
-    );
-}
+import { PanelBuildProcess } from './panel-build-process';
 
 function HeaderTitle() {
     const sawContentStr = useAtomValue(sawContentStrAtom);
---
+++ src/components/section2-main/panel-window-content/header/panel-build-process.tsx
@@ -0,0 +1,47 @@
+import { useSnapshot } from "valtio";
+import { sendToMain } from "@/store";
+import { clientState, buildState } from "@/store/app-state";
+
+const buttonClasses = "px-2 py-1 border-primary-500 hover:border-primary-600 hover:bg-primary-500 border rounded shadow active:scale-[.97] disabled:scale-100 disabled:hover:bg-transparent disabled:opacity-20 transition-transform";
+
+function BuildCounter() {
+    const { buildError } = useSnapshot(clientState);
+    const { buildCounter } = useSnapshot(buildState);
+    if (buildError || buildCounter < 200) {
+        return null;
+    }
+    return (<>
+        <div className="pt-0.5">
+            controls detection progress
+        </div>
+
+        <div className="pt-0.5 min-w-[2.5rem] font-semibold font-mono">
+            {buildCounter}
+        </div>
+
+        <button className={buttonClasses} onClick={() => sendToMain({ type: 'cancel-detection' })}>
+            Cancel
+        </button>
+    </>);
+}
+
+function BuildError() {
+    const { buildError } = useSnapshot(clientState);
+    if (!buildError) {
+        return null;
+    }
+    return (
+        <div className="px-2 py-1 bg-red-600 text-white rounded-sm">
+            {buildError}
+        </div>
+    );
+}
+
+export function PanelBuildProcess() {
+    return (
+        <div className="text-xs text-primary-700 flex items-center gap-x-1">
+            <BuildCounter />
+            <BuildError />
+        </div>
+    );
+}

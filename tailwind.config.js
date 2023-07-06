46656689ea74579bdbfb4da576605be620c818ab
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-05 18:27:45 -0700

    WIP. Section3Footer. add other elements to fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,23 @@ const svgToDataUri = require('mini-svg-data-uri');
 
 const fixInputs = plugin(
     function ({ addComponents }) {
+        const markColor = 'red'
         addComponents({
             '.form-checkbox:checked': {
-                '--color': 'red',
                 'background-image': `url("${svgToDataUri(
-                    `<svg viewBox="0 0 16 16" fill="var(--color, white)" xmlns="http://www.w3.org/2000/svg"><path d="M12.207 4.793a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-2-2a1 1 0 011.414-1.414L6.5 9.086l4.293-4.293a1 1 0 011.414 0z"/></svg>`
+                    `<svg viewBox="0 0 16 16" fill="${markColor}" xmlns="http://www.w3.org/2000/svg"><path d="M12.207 4.793a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-2-2a1 1 0 011.414-1.414L6.5 9.086l4.293-4.293a1 1 0 011.414 0z"/></svg>`
                   )}")`,
-            }
+            },
+            '.form-checkbox:indeterminate': {
+                'background-image': `url("${svgToDataUri(
+                    `<svg xmlns="http://www.w3.org/2000/svg" fill="${markColor}" viewBox="0 0 16 16"><path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h8"/></svg>`
+                  )}")`,
+            },
+            '.form-radio:checked': {
+                'background-image': `url("${svgToDataUri(
+                    `<svg viewBox="0 0 16 16" fill="${markColor}" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="3"/></svg>`
+                  )}")`,
+            },
         });
     },
 );

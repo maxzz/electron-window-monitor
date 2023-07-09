9922e0453071cc3afa60f124a15248b17877e998
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-08 19:39:18 -0700

    WIP. ControlsGrid. add icon by type to engine control

--- src/components/section2-main/panel-window-content/body-elements/content-items.tsx
+++ src/components/section2-main/panel-window-content/body-elements/content-items.tsx
@@ -1,24 +1,27 @@
-import { EngineControl } from "@/electron/app/napi-calls";
-import { FormRowTypeIcon, engineControlToFieldIconTypes } from "@/store/manifest/manifest-fields";
+import { EngineControlWithMeta } from "@/store";
+import { FormRowTypeIcon, engineControlToFieldIconTypes } from "@/store/manifest";
 import { classNames } from '@/utils';
 
-export function ControlsGridItem({ control }: { control: EngineControl; }) {
+export function ControlsGridItem({ item }: { item: EngineControlWithMeta; }) {
     return (<>
-        <div className="text-end !pl-1 border-none">{control.memid}</div>
-        <div className="text-end">{control.orderid}</div>
-        <div className=""><FormRowTypeIcon field={engineControlToFieldIconTypes(control)} className="w-4 h-4" /></div>
-        <div className="max-w-[30ch] whitespace-nowrap overflow-hidden overflow-ellipsis">{control.dispname}</div>
-        <div className="text-[.6rem]">{control.path}</div>
+        <div className="text-end !pl-1 border-none">{item.control.memid}</div>
+        <div className="text-end">{item.control.orderid}</div>
+        <div className=""><FormRowTypeIcon field={engineControlToFieldIconTypes(item.control)} className="w-4 h-4" /></div>
+        <div className="max-w-[30ch] whitespace-nowrap overflow-hidden overflow-ellipsis">{item.control.dispname}</div>
+        <div className="text-[.6rem]">{item.control.path}</div>
     </>);
 }
 
 const vlineClasses = "[&>*]:py-0.5 [&>*]:border-primary-500 [&>*]:border-l [&>*]:pl-2"; // [&>*~*]:border-b
 
-export function ControlsGrid({ controls }: { controls: EngineControl[]; }) {
+export function ControlsGrid({ controls }: { controls: EngineControlWithMeta[]; }) {
+    
+    console.log('controls', controls);
+    
     return (
         <div className={classNames("text-xs grid grid-cols-[repeat(5,min-content)] gap-x-2 overflow-auto", vlineClasses,)}>
             {controls.map((control, idx) => (
-                <ControlsGridItem control={control} key={idx} />
+                <ControlsGridItem item={control} key={idx} />
             ))}
         </div>
     );

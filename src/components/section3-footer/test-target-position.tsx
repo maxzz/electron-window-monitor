7d13c22ff3c4b157a337ab11619143cf15e1752a
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-11 19:01:01 -0700

    WIP. TestTargetWindowPosition. update target icon

--- src/components/section3-footer/test-target-position.tsx
+++ src/components/section3-footer/test-target-position.tsx
@@ -1,23 +1,28 @@
 import { useSnapshot } from "valtio";
-import { IconTarget } from "../ui/icons";
+import { IconTarget2 } from "../ui/icons";
 import { buildState } from "@/store/app-state";
 import { invokeMain, sawHandleAtom } from "@/store";
 import { useAtomValue } from "jotai";
 
 export function TestTargetWindowPosition() {
     const { getPosProgress } = useSnapshot(buildState);
+    
     const sawHandle = useAtomValue(sawHandleAtom);
     if (!sawHandle?.hwnd) {
         return null;
     }
 
+    function startDragging() {
+        if (sawHandle?.hwnd) {
+            //invokeMain({ type: 'get-window-pos', hwnd: sawHandle.hwnd });
+        }
+    }
+
     return (
         <div className="flex items-end space-x-2">
             <div className="">Test get target position:</div>
-            <div className="w-12 h-12 bg-primary-900 rounded" onMouseDown={() => {
-                invokeMain({ type: 'get-window-pos', hwnd: sawHandle?.hwnd });
-            }}>
-                <IconTarget className="text-primary-200" />
+            <div className="w-12 h-12 bg-primary-900 rounded" onMouseDown={startDragging}>
+                <IconTarget2 className="text-primary-200" />
             </div>
             {getPosProgress &&
                 <div className="">{getPosProgress.point.x}, {getPosProgress.point.y}</div>
--- src/components/ui/icons/normal/index.tsx
+++ src/components/ui/icons/normal/index.tsx
@@ -2,3 +2,4 @@ export * from './icon01-copy';
 export * from './icon02-play';
 export * from './icon03-stop';
 export * from './icon04-target';
+export * from './icon05-target2';

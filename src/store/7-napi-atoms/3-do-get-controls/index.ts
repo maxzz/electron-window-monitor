5b034af9eeedb4b136fc6ca38c6a66ea422769ca
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-04 18:41:19 -0800

    WIP. update 7-napi-atoms layout. 3

--- src/store/7-napi-atoms/3-do-get-controls/index.ts
+++ src/store/7-napi-atoms/3-do-get-controls/index.ts
@@ -1,1 +1,2 @@
 export * from "./0-all/3-do-get-controls";
+export * from "./controls-meta";
--- src/store/7-napi-atoms/index.ts
+++ src/store/7-napi-atoms/index.ts
@@ -1,9 +1,8 @@
 export * from './1-do-get-hwnd';
 export * from './2-do-get-icon';
 export * from './3-do-get-controls';
-export * from './3-do-get-controls/controls-meta';
 export * from './4-do-get-mani';
-export * from './8-do-monitoring';
 export * from './6-do-highlight-rect';
+export * from './8-do-monitoring';
 
 // Abbreviations: SAW - Second Active Window

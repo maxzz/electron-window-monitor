705400aaac3ed2abab3e2c26a4338d723cdeb297
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-04 19:22:44 -0800

    WIP. update 7-napi-atoms layout. 7

--- src/store/7-napi-atoms/3-do-get-controls/index.ts
+++ src/store/7-napi-atoms/3-do-get-controls/index.ts
@@ -1,3 +1,3 @@
 export * from "./9-types";
-export * from "./0-all/3-do-get-controls";
-export * from "./controls-meta";
+export * from "./0-all/1-do-get-controls";
+export * from "./2-conv-controls-meta";

a2cf53010548929e26b6b864afabc0d84d8b72e8
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-08 16:58:11 -0800

    WIP. update app layout. 7

--- src/components/2-main/0-all/index.tsx
+++ src/components/2-main/0-all/index.tsx
@@ -7,11 +7,11 @@ import { SawControlsPanel } from "../3-panel-window-content";
 export function Section2Main({ className, ...rest }: ComponentPropsWithoutRef<"div">) {
     return (
         <div className={classNames("min-h-0 text-primary-900 flex flex-col gap-4 divide-y-1 divide-sky-500/50", className)} {...rest}>
-            <StartActionsPanel className="py-4 border-b border-primary-500/50" />
+            <StartActionsPanel className="py-4 1border-b border-primary-500/50" />
 
-            <SawHwndInfo className="border-b border-primary-500/50" />
+            <SawHwndInfo className="border-b 1border-primary-500/50" />
 
-            <SawControlsPanel className="border-b border-primary-500/50" />
+            <SawControlsPanel className="border-b 1border-primary-500/50" />
         </div>
     );
 } 

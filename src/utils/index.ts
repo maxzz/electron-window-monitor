67ac4238d870ea46beb803887c8662418b7ac6ca
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-12 18:22:42 -0800

    WIP. build. add getSubError()

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,13 +3,4 @@ export * from './web-data-transfer-item-list';
 export * from './web-drop-utils';
 export * from './merge-options';
 export * from './prepend-url-path';
-
-/**
- * Get from error message
- * "'get-saw-content' Error invoking remote method 'invoke-main': >>>Too many controls"
- * string 'Too many controls'.
- */
-export function getSubError(error: unknown) {
-    const msg = error instanceof Error ? error.message : `${error}`;
-    return msg.split('>>>').at(-1) || msg;
-}
+export * from './x-sub-error';

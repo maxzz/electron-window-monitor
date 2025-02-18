a861ad79cbfd486f744f3aba21c4aa4cf9fe2d94
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-17 16:41:43 -0800

    WIP. build. update main-to-renderer calls

--- src/shared/ipc-client/ipc-react-listener-atom.ts
+++ src/shared/ipc-client/ipc-react-listener-atom.ts
@@ -7,27 +7,27 @@ export const doFromMainAtom = atom(
     null,
     (get, set, data: M2R.RendererCalls) => {
         switch (data.type) {
-            case 'dark-mode': {
-                console.log('case dark-mode, active', data.active);
+            case 'm2r:dark-mode': {
+                console.log('case m2r:dark-mode, active', data.active);
                 break;
             }
-            case 'reload-files': {
+            case 'm2r:reload-files': {
                 console.log('reload-files');
                 break;
             }
-            case 'opened-files': {
+            case 'm2r:opened-files': {
                 set(filesContentAtom, data.filesCnt);
                 break;
             }
-            case 'detection-progress': {
+            case 'm2r:detection-progress': {
                 napiBuildProgress.buildCounter = data.progress;
                 break;
             }
-            case 'position-progress': {
+            case 'm2r:position-progress': {
                 napiBuildProgress.getPosProgress = data.progress;
                 break;
             }
-            case 'failed-raw-content': {
+            case 'm2r:failed-raw-content': {
                 napiBuildState.buildFailedBody = data.body;
                 break;
             }

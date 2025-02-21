42bafce755893b6080adbbd322e2d39f865a8f8a
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-20 18:35:43 -0800

    WIP. monitor. add ButtonGetSawHandle presence animation

--- src/components/2-main/1-panel-start-actions/5-btn-get-saw-handle.tsx
+++ src/components/2-main/1-panel-start-actions/5-btn-get-saw-handle.tsx
@@ -1,17 +1,40 @@
-import { type HTMLAttributes } from "react";
+import { type ComponentPropsWithoutRef, type HTMLAttributes } from "react";
 import { useSetAtom, useAtomValue } from "jotai";
+import { AnimatePresence, motion, type Transition } from "motion/react";
 import { classNames } from "@/utils";
-import { doGetTargetHwndAtom, doMonitoringAtom } from "@/store";
 import { buttonClasses } from "./8-button-classes";
+import { doGetTargetHwndAtom, doMonitoringAtom } from "@/store";
 
-export function ButtonGetSawHandle({ className, ...rest }: HTMLAttributes<HTMLButtonElement>) {
+export function ButtonGetSawHandle({ className, ...rest }: ComponentPropsWithoutRef<typeof motion.button>) {
 
     const isMonitoring = useAtomValue(doMonitoringAtom);
     const doGetTargetHwnd = useSetAtom(doGetTargetHwndAtom);
 
     return (
-        <button className={classNames(buttonClasses, isMonitoring && "!opacity-5", className)} disabled={isMonitoring} onClick={doGetTargetHwnd} {...rest}>
-            Get Second Window
-        </button>
+        <AnimatePresence>
+            {!isMonitoring && (
+                <motion.button
+                    initial={{ opacity: 0, scale: 0.95 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0.95 }}
+                    transition={animationTransition}
+                    className={classNames(buttonClasses,
+                        //isMonitoring && "!opacity-5", 
+                        className)}
+                    disabled={isMonitoring}
+                    onClick={doGetTargetHwnd}
+                    {...rest}
+                >
+                    Get Second Window
+                </motion.button>
+            )}
+        </AnimatePresence>
     );
 }
+
+const animationTransition: Transition = {
+    // type: "spring",
+    // stiffness: 500,
+    // damping: 50,
+    duration: 0.2,
+};

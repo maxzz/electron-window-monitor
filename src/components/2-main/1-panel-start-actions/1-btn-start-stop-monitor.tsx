05504716ca40ec9bc68efb8719862ff9e33e3eb9
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-20 17:58:13 -0800

    WIP. monitor. doMonitoringAtom code review. 4

--- src/components/2-main/1-panel-start-actions/1-btn-start-stop-monitor.tsx
+++ src/components/2-main/1-panel-start-actions/1-btn-start-stop-monitor.tsx
@@ -18,18 +18,10 @@ export function ButtonStartStopMonitor() {
 
     return (
         <button className={classNames("relative", buttonClasses)} onClick={sendRequest}>
-            {isMonitoring
-                ? (
-                    <div className="flex items-center gap-1"><IconPlayStop className="w-4 h-4 pt-0.5 fill-red-500 text-red-400" />
-                        Stop Monitor
-                    </div>
-                )
-                : (
-                    <div className="flex items-center gap-1"><IconPlayStart className="w-4 h-4 pt-0.5" />
-                        Start Monitor
-                    </div>
-                )
-            }
+            <div className="flex items-center gap-1"><IconPlayStop className={classNames("pt-0.5 size-4", isMonitoring && "fill-red-500 text-red-400")} />
+                {isMonitoring ? 'Stop Monitor' : 'Start Monitor'}
+            </div>
+
             <MonitorCounter className="absolute -top-3 px-2 leading-6 bg-primary-400 border-primary-600 border rounded" />
         </button>
     );
@@ -41,7 +33,7 @@ function MonitorCounter({ className, ...rest }: HTMLAttributes<HTMLDivElement>) 
     if (monitorCounter < 0) {
         return null;
     }
-    
+
     return (
         <div className={classNames(counterClasses, className)} title="Number of calls to check the active window" {...rest}>
             {`${monitorCounter}`.padStart(2, '0')}

9ed72927f307460df6c8421d39530db1473cfd5c
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-23 14:35:26 -0800

    WIP. monitor. add IconEyes. v5.

--- src/components/2-main/1-panel-start-actions/1-btn-start-stop-monitor.tsx
+++ src/components/2-main/1-panel-start-actions/1-btn-start-stop-monitor.tsx
@@ -5,11 +5,13 @@ import { classNames } from "@/utils";
 import { animationProps, animationTransition, buttonClasses } from "./8-button-classes";
 import { IconPlayStop, IconPlayStart } from "@/components/ui";
 import { doGetTargetHwndAtom, doMonitoringAtom, monitorCounterAtom } from "@/store";
+
 import { IconRadarV1 } from "@/components/ui/icons/animated/radar-v1";
 import { IconEyes } from "@/components/ui/icons/animated/eyes";
 import { EyesFollowCursor } from "@/components/ui/icons/animated/eyes-v2";
 import { EyeV3 } from "@/components/ui/icons/animated/eyes-v3";
 import { EyeV4 } from "@/components/ui/icons/animated/eyes-v4";
+import { EyeV5 } from "@/components/ui/icons/animated/eyes-v5";
 
 export function ButtonStartStopMonitor() {
     const [isMonitoring, setIsMonitoring] = useAtom(doMonitoringAtom);
@@ -30,7 +32,8 @@ export function ButtonStartStopMonitor() {
             {/* <EyesFollowCursor /> */}
             {/* <IconEyes /> */}
             {/* <EyeV3 /> */}
-            <EyeV4 />
+            {/* <EyeV4 /> */}
+            <EyeV5 />
         </button>
     );
 }

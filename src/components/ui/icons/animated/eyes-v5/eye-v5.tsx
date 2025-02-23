1c87cd380ddf3d234a24ff971a48b1f057659658
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-23 14:41:07 -0800

    WIP. monitor. add IconEyes. v5. code review

--- src/components/ui/icons/animated/eyes-v5/eye-v5.tsx
+++ src/components/ui/icons/animated/eyes-v5/eye-v5.tsx
@@ -4,21 +4,21 @@ import { useEffect, useState } from 'react'; //https://github.com/Kuldeepsinghra
 export function EyeV5() {
     const [Rotate, setRotate] = useState<number>(0);
 
-    useEffect(() => {
-        const handleMouseMove = (e: MouseEvent) => {
-            let mousex: number = e.clientX;
-            let mousey: number = e.clientY;
-            let deltaX: number = mousex - window.innerWidth / 2;
-            let deltaY: number = mousey - window.innerHeight / 2;
+    useEffect(
+        () => {
+            function handleMouseMove(e: MouseEvent) {
+                let mousex: number = e.clientX;
+                let mousey: number = e.clientY;
+                let deltaX: number = mousex - window.innerWidth / 2;
+                let deltaY: number = mousey - window.innerHeight / 2;
 
-            let Rotate: number = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
-            setRotate(Rotate + 180);
-        };
+                let Rotate: number = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+                setRotate(Rotate + 180);
+            }
 
-        window.addEventListener('mousemove', handleMouseMove);
-
-
-    }, []);
+            window.addEventListener('mousemove', handleMouseMove);
+        }, []
+    );
 
     // Eye section
     console.log(Rotate);
@@ -26,14 +26,15 @@ export function EyeV5() {
     return (
         <div className="1h-screen relative 1w-full overflow-hidden">
             <div data-scroll data-scroll-speed="-0.7" className='relative h-full flex justify-center items-center text-center  bg-cover w-full 1bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg")]'>
-                <div className="flex justify-between gap-20   rounded-full">
-                    <div className="bg-white rounded-full  flex justify-center items-center h-[15rem] w-[15rem]">
-                        <div className="bg-black  flex justify-center items-center rounded-full h-[10rem] w-[10rem]">
-                            <div className='absolute uppercase text-4xl  font-bold'>play</div>
-                            <div className={` h-full w-full flex justify-center  items-center`}>
 
-                                <div style={{ transform: `translate(-0%,-0%) rotate(${Rotate}deg)` }} className={`lineforcircle  w-1 h-full     `}>
-                                    <div className={`bg-white  text-  w-[2rem] h-[2rem] flex justify-center rotate-[${Rotate}deg]  rounded-full`}>
+                <div className="flex justify-between gap-20   rounded-full">
+                    <div className="size-[15rem] bg-white rounded-full  flex justify-center items-center">
+                        <div className="size-[10rem] bg-black flex justify-center items-center rounded-full">
+                            <div className='absolute uppercase text-4xl  font-bold'>play</div>
+
+                            <div className="size-full flex justify-center  items-center">
+                                <div className="w-1 h-full lineforcircle" style={{ transform: `translate(-0%,-0%) rotate(${Rotate}deg)` }}>
+                                    <div className={`size-[2rem] bg-white flex justify-center rotate-[${Rotate}deg] rounded-full`}>
                                         {/* PLAY */}
                                     </div>
                                 </div>
@@ -41,19 +42,21 @@ export function EyeV5() {
                         </div>
                     </div>
 
-                    <div className="bg-white rounded-full  flex justify-center items-center h-[15rem] w-[15rem]">
-                        <div className="bg-black  flex justify-center items-center rounded-full h-[10rem] w-[10rem]">
+                    <div className="size-[15rem] bg-white rounded-full  flex justify-center items-center">
+                        <div className="size-[10rem] bg-black flex justify-center items-center rounded-full">
                             <div className='absolute uppercase text-4xl  font-bold'>play</div>
 
-                            <div className={` h-full w-full flex justify-center  items-center`}>
-
-                                <div style={{ transform: `translate(-0%,-0%) rotate(${Rotate}deg)` }} className={`lineforcircle  w-1 h-full     `}>
-                                    <div className={`bg-white   w-[2rem] h-[2rem] flex justify-center rotate-[${Rotate}deg]  rounded-full`}></div>
+                            <div className="size-full flex justify-center  items-center">
+                                <div className="w-1 h-full lineforcircle" style={{ transform: `translate(-0%,-0%) rotate(${Rotate}deg)` }}>
+                                    <div className={`size-[2rem] bg-white flex justify-center rotate-[${Rotate}deg] rounded-full`}>
+                                        {/* PLAY */}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     );

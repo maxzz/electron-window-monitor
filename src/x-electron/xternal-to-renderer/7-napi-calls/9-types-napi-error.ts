26b1451a70a334953f9af330f40bcaf514f9ae55
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-10 18:44:40 -0700

    WIP. sync w/ PMAT. error handling. 5. add splitTypedError()

--- src/x-electron/xternal-to-renderer/7-napi-calls/9-types-napi-error.ts
+++ src/x-electron/xternal-to-renderer/7-napi-calls/9-types-napi-error.ts
@@ -1,4 +1,5 @@
 export type NapiCallError =
+    | 'undefined'           // Undefined error. used during error split
     | 'unknown-error'       // Unknown error; see extra param for details; as usual from catch
     | 'build-error'         // Error during manifest build
     | 'build-wo-mani'       // Build done without manifest
@@ -22,3 +23,41 @@ export function errorToString(error: unknown): string {
     }
     return `${error}`;
 }
+
+export function splitTypedError(errorStr: string): { error: NapiCallError, extra: string | undefined; } {
+    const typed = errorStr.split('>>>').at(-1) as NapiCallError;
+
+    if (!typed) {
+        return {
+            error: 'undefined',
+            extra: errorStr,
+        };
+    }
+
+    const parts = typed.split('...');
+    return {
+        error: parts[0] as NapiCallError,
+        extra: parts[1],
+    };
+}
+
+// function splitTypedError(errorStr: string): { error: NapiCallError, extra?: string; } {
+//     const parts = errorStr.split(':::');
+//     const error = parts[0] as NapiCallError;
+
+//     if (parts.length === 1) {
+//         return {
+//             error,
+//         };
+//     } else if (parts.length === 2) {
+//         return {
+//             error,
+//             extra: parts[1],
+//         };
+//     } else {
+//         return {
+//             error: 'undefined',
+//             extra: errorStr,
+//         };
+//     }
+// }

f5af6e07701ec022e4ca86f215238c3ad54b2249
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-11 17:40:35 -0700

    WIP. sync w/ PMAT. update doGetWindowIconAtom

--- src/store/7-napi-atoms/2-do-get-icon/2-do-get-icon.ts
+++ src/store/7-napi-atoms/2-do-get-icon/2-do-get-icon.ts
@@ -1,7 +1,7 @@
-import { atom } from "jotai";
-import { invokeMain } from "@/shared/2-gates-in-client-as-atoms";
+import { atom, type Getter, type Setter } from "jotai";
+import { hasMain, invokeMain } from "@/shared/2-gates-in-client-as-atoms";
 import { type WindowIconGetterResult } from "@/x-electron/xternal-to-renderer/7-napi-calls";
-import { napiBuildState } from "../9-napi-build-state";
+import { napiBuildState, napiLock } from "../9-napi-build-state";
 import { errorToString, splitTypedError, typedErrorToString } from "@/utils";
 //import { sawHandleAtom } from "./do-get-hwnd";
 
@@ -9,48 +9,94 @@ export const sawIconStrAtom = atom<string | undefined>(undefined);
 export const sawIconAtom = atom<HTMLImageElement | null>(null);
 export type SawIconAtom = typeof sawIconAtom;
 
-type IconsCache = Map<string, string>; // hwnd -> string with WindowIconGetterResult
-
-const iconsCache: IconsCache = new Map();
+export const doClearSawIconAtom = atom(
+    null,
+    (get, set) => {
+        set(sawIconStrAtom, '');
+        set(sawIconAtom, null);
+    }
+);
 
 export const doGetWindowIconAtom = atom(
     null,
     async (get, set, hwnd: string | undefined): Promise<void> => {
-        try {
-            if (!hwnd) {
-                throw new Error('No hwnd');
-            }
+        if (!hwnd) {
+            set(doClearSawIconAtom);
+            return;
+        }
 
-            const cached = iconsCache.get(hwnd);
-
-            const str = cached ? cached : await invokeMain<string>({ type: 'r2mi:get-window-icon', hwnd });
-
-            if (str && str !== cached) {
-                iconsCache.set(hwnd, str);
-            }
-
-            const prev = get(sawIconStrAtom);
-            if (prev !== str) {
-                set(sawIconStrAtom, str);
-
-                const res = JSON.parse(str || '') as WindowIconGetterResult;
-                const image = new Image();
-                image.src = `data:image/png;base64,${res.data}`;
-                set(sawIconAtom, image);
-            }
-
-            napiBuildState.buildError = '';
-
-            //console.log('doGetSawIconAtom.set', JSON.stringify(str, null, 4));
-        } catch (error) {
-            set(sawIconStrAtom, '');
-            set(sawIconAtom, null);
-
-            napiBuildState.buildError = errorToString(error);
-            console.error(`'doGetWindowIconAtom' ${typedErrorToString(splitTypedError(napiBuildState.buildError))}`);
+        if (!napiLock.locked()) {
+            hasMain()
+                ? await doLiveIcon(hwnd, get, set)
+                : await doTestIcon(hwnd, get, set);
+            napiLock.unlock();
         }
     }
 );
+
+async function doLiveIcon(hwnd: string, get: Getter, set: Setter) {
+    try {
+        if (!hwnd) {
+            throw new Error('No hwnd');
+        }
+
+        const cached = iconsCache.get(hwnd);
+
+        const str = cached ? cached : await invokeMain<string>({ type: 'r2mi:get-window-icon', hwnd });
+
+        if (str && str !== cached) {
+            iconsCache.set(hwnd, str);
+        }
+
+        const prev = get(sawIconStrAtom);
+        if (prev !== str) {
+            set(sawIconStrAtom, str);
+
+            const res = JSON.parse(str || '') as WindowIconGetterResult;
+            const image = new Image();
+            image.src = `data:image/png;base64,${res.data}`;
+            set(sawIconAtom, image);
+        }
+
+        napiBuildState.buildError = '';
+
+        //console.log('doGetSawIconAtom.set', JSON.stringify(str, null, 4));
+    } catch (error) {
+        set(sawIconStrAtom, '');
+        set(sawIconAtom, null);
+
+        napiBuildState.buildError = errorToString(error);
+        console.error(`'doGetWindowIconAtom' ${typedErrorToString(splitTypedError(napiBuildState.buildError))}`);
+    }
+}
+
+const iconsCache: Map<string, string> = new Map(); // hwnd -> string with WindowIconGetterResult
+
+export function clearIconsCache() { // Clear icons cache on monitor dialog close
+    iconsCache.clear();
+}
+
+async function doTestIcon(hwnd: string, get: Getter, set: Setter) {
+    // // if (lastTestCreateHwnd === debugSettings.testCreate.hwnd) {
+    // //     return;
+    // // }
+    // // lastTestCreateHwnd = debugSettings.testCreate.hwnd;
+
+    // const testHwnd = (await set(doLoadFakeHwndAtom, debugSettings.testCreate.hwnd)) as unknown as TestHwnd;
+
+    // if (testHwnd?.icon?.data) {
+    //     const image = new Image();
+    //     image.src = `data:image/png;base64,${testHwnd.icon.data}`;
+    //     set(sawIconAtom, image);
+    //     set(sawIconStrAtom, JSON.stringify(testHwnd));
+    // } else {
+    //     set(doClearSawIconAtom);
+    // }
+}
+
+// let lastTestCreateHwnd: typeof debugSettings.testCreate.hwnd = 'none';
+
+//
 
 /* export const currentWindowIconAtom = atom(
     (get) => {

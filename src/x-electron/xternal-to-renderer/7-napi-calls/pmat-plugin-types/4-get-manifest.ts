804d24d725aa966c2dc5752fd7ee80ed617fb1a2
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-13 19:23:13 -0700

    WIP. add tp lugin-types flag manual

--- src/x-electron/xternal-to-renderer/7-napi-calls/pmat-plugin-types/4-get-manifest.ts
+++ src/x-electron/xternal-to-renderer/7-napi-calls/pmat-plugin-types/4-get-manifest.ts
@@ -4,6 +4,8 @@ import { type WindowControlsCollectProgress } from ".";
 export type ManifestForWindowCreatorParams = {
     hwnd: string;
     wantXml: boolean;
+    manual: boolean;               // return window top-level match info without checking controls
+    passwordChange: boolean;       // create manifest for password change
 };
 
 export type ManifestCreationDataResult = {

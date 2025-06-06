75cb27911043f868a91df176a7a24aba18ba5b25
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-05 17:02:43 -0700

    WIP. update package dependencies. plugin binaries

--- src/x-electron/xternal-to-renderer/7-napi-calls/pmat-plugin-types/0-all.ts
+++ src/x-electron/xternal-to-renderer/7-napi-calls/pmat-plugin-types/0-all.ts
@@ -3,6 +3,7 @@ import { type WindowIconGetter } from "./2-get-icon";
 import { type WindowControlsCollector } from "./3-get-controls";
 import { type ManifestForWindowCreator } from "./4-get-manifest";
 import { type GetTlwInfos } from "./5-get-screenshots";
+import { type GetWindowExtras } from "./8-extra-info";
 import { type GetTlwScreenshots } from "./5-get-screenshots";
 import { type WindowControlHighlighter } from "./6-highlight-control";
 import { type DragAndDrop } from "./7-drag-and-drop";
@@ -15,5 +16,6 @@ export type AddonTypes = {
     ManifestForWindowCreator: ManifestForWindowCreator;
     WindowControlHighlighter: WindowControlHighlighter;
     getTopLevelWindowsInfo: GetTlwInfos;
+    getTopLevelWindowsExtraInfo: GetWindowExtras;
     getTopLevelWindowsScreenshots: GetTlwScreenshots;
 };

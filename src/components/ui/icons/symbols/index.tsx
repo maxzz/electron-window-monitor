39d46fe6882fc8eae4750cd48186f47c079e3db6
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-07 14:16:12 -0700

    WIP. top-controls. add symbol icons

--- src/components/ui/icons/symbols/index.tsx
+++ src/components/ui/icons/symbols/index.tsx
@@ -1,12 +1,15 @@
 import { UISymbolDefsInject } from 'pm-manifest-icons';
 import { DefFieldTypes } from 'pm-manifest-icons/src/symbols/field';
+import { DefAllOtherTypes } from './all-other';
 
 export * from 'pm-manifest-icons/src/symbols/field';
+export * from "./all-other";
 
 export function UISymbolDefs() {
     return (
         <UISymbolDefsInject>
             {DefFieldTypes()}
+            {DefAllOtherTypes()}
         </UISymbolDefsInject>
     );
 }

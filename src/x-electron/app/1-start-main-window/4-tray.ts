622b0a95c060592328c81a41ad223456d67614c7
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-20 15:04:32 -0700

    WIP. electron. avg tray icon. no

--- src/x-electron/app/1-start-main-window/4-tray.ts
+++ src/x-electron/app/1-start-main-window/4-tray.ts
@@ -1,12 +1,18 @@
 import path from "node:path";
-import { app, Tray } from "electron";
+import { app, nativeTheme, Tray } from "electron";
 import { appWindow } from "./7-app-window-instance";
+import { log } from "node:console";
 
 //import TrayIconDarkWin from '../../resources/trayDark.ico?asset';
+import TrayIconDarkWin from '../../../assets/icons/favicon.svg?asset';
+
+const isDarkMode = nativeTheme.shouldUseDarkColors;
+log('----------isDarkMode', isDarkMode);
 
 export function createTray() {
     // Create the tray icon
-    const tray = new Tray(path.join(app.getAppPath(), 'resources', 'icon.png'));
+    // const tray = new Tray(path.join(app.getAppPath(), 'resources', 'icon.png'));
+    const tray = new Tray(TrayIconDarkWin);
 
     // Set the context menu for the tray icon
     tray.setToolTip('Win-Mon');

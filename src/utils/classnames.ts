cc624591ad0c0a9fbb1d984ba607e50b3faf708b
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-26 19:28:12 -0700

    WIP. customize components.json

--- src/utils/classnames.ts
+++ src/utils/classnames.ts
@@ -1,10 +1,14 @@
 import { twMerge } from "tailwind-merge";
 
 export function cx(...classes: Array<string | Record<string, boolean>>): string {
-    return classes.map((cls) => (
-        typeof cls === 'string'
-            ? cls
-            : Object.keys(cls).filter((subKey: string) => cls[subKey]).join(' ')),
+    return classes.map(
+        (cls) => (
+            typeof cls === 'string'
+                ? cls
+                : Object.keys(cls).filter(
+                    (subKey: string) => cls[subKey]
+                ).join(' ')
+        ),
     ).join(' ');
 }
 

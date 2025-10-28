28382f4615498c99a3ac6ee32d28ff2a2eebd96a
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-27 18:06:59 -0700

    WIP. accordion. add test. 2

--- src/components/2-main/0-all/index.tsx
+++ src/components/2-main/0-all/index.tsx
@@ -3,7 +3,7 @@ import { classNames } from "@/utils";
 import { StartActionsPanel } from "../1-panel-start-actions";
 import { SawHwndInfo } from "../2-panel-saw-hwnd-handle";
 import { SawControlsPanel } from "../3-panel-window-content";
-import { PopoverDemo } from "@/components/ui/ui-tests";
+import { AccordionDemo, PopoverDemo } from "@/components/ui/ui-tests";
 
 export function Section2Main({ className, ...rest }: ComponentPropsWithoutRef<"div">) {
     return (
@@ -12,6 +12,7 @@ export function Section2Main({ className, ...rest }: ComponentPropsWithoutRef<"d
 
             <SawHwndInfo className="border-b 1border-primary-500/50" />
             <PopoverDemo />
+            <AccordionDemo />
 
             <SawControlsPanel className="border-b 1border-primary-500/50" />
         </div>

ecb2e7cd2d828ada18254a9fca68500d72df23f2
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-13 17:04:42 -0800

    WIP. top-menu. fix menu trigger

--- src/components/1-header/1-top-menu/0-all.tsx
+++ src/components/1-header/1-top-menu/0-all.tsx
@@ -4,32 +4,32 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel,
 
 export function TopMenu() {
     return (
-        //TODO: add drop down menu with the following items:
-        // - sub menu "Test links" with the following items:
-        //  - "Test link 1"
-        //  - "Test link 2"
-        // the real links are taken from the @/components/3-footer/1-controls-bar/2-test-links.tsx component
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="icon" className="size-6" title="Options">
+                <Button className="size-6 focus-visible:ring-0" variant="ghost" size="icon" title="Options">
                     <IconMenuHamburger5 className="size-5" />
                 </Button>
             </DropdownMenuTrigger>
 
-            <DropdownMenuContent>
-                {/* TODO: add DropdownMenu with items from testLinks */}
+            <DropdownMenuContent className="overflow-hidden">
                 <DropdownMenuSub>
-                    <DropdownMenuSubTrigger>
-                        <DropdownMenuLabel>Test links</DropdownMenuLabel>
+                    <DropdownMenuSubTrigger className="py-0">
+                        <DropdownMenuLabel className="text-xs font-normal">Test links</DropdownMenuLabel>
                     </DropdownMenuSubTrigger>
+
+                    <DropdownMenuSubContent>
+                        {testLinks.map(
+                            (link) => (
+                                <DropdownMenuItem className="text-xs font-normal" key={link.href}>
+                                    <a href={link.href} target="_blank">
+                                        {link.label}
+                                    </a>
+                                </DropdownMenuItem>
+                            )
+                        )}
+                    </DropdownMenuSubContent>
                 </DropdownMenuSub>
-                <DropdownMenuSubContent>
-                    {testLinks.map((link) => (
-                        <DropdownMenuItem key={link.href}>
-                            <a href={link.href} target="_blank">{link.label}</a>
-                        </DropdownMenuItem>
-                    ))}
-                </DropdownMenuSubContent>
+
             </DropdownMenuContent>
         </DropdownMenu>
     );
@@ -37,11 +37,11 @@ export function TopMenu() {
 
 const testLinks = [
     {
-        label: "Test link 1",
-        href: "https://www.google.com"
+        label: "Tailwind UI login",
+        href: "https://tailwindui.com/login"
     },
     {
-        label: "Test link 1",
-        href: "https://www.google.com"
+        label: "Bank of America login",
+        href: "https://secure.bankofamerica.com/login/sign-in/signOnV2Screen.go"
     },
 ];

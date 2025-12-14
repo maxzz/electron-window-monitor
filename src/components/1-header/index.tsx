2c0f89fc9dfdbdc43abf8c147133e78631f2adbd
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-13 16:16:35 -0800

    WIP. update package dependencies. pnpx shadcn@latest add dialog dropdown-menu scroll-area select sonner switch tooltip tabs slider resizable radio-group context-menu collapsible

--- src/components/1-header/index.tsx
+++ src/components/1-header/index.tsx
@@ -5,10 +5,11 @@ import { IconMicroscope } from "../ui/icons/normal";
 
 export function Section1Header({ className, ...rest }: ComponentPropsWithoutRef<"div">) {
     return (
-        <div className={classNames("px-2 py-2 border-b border-border shadow-xs", className)} {...rest}>
+        <div className={classNames("pl-4 pr-2 py-2 border-b border-border shadow-xs", className)} {...rest}>
 
             <div className="flex items-center justify-between">
                 <IconMicroscope className="size-4 stroke-6! text-foreground/50" />
+                <span className="text-xs">Second active window (SAW)</span>
                 <PopoverSettings />
             </div>
 
